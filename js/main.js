6a9c1a7fbb99d66d706a1c18aa88a2a9c155d353
unknown <devmonikamomi@gmail.com>
2021-05-22 22:15:29 -0700

    travel website

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    item:1,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+   
+    
+})
+
+
+
+});
+
+document.getElementById('search').addEventListener("click",function(){
+    document.querySelector(".popUp").style.display = "flex";
+})
+
+document.querySelector(".close").addEventListener("click",function(){
+    document.querySelector(".popUp").style.display = "none";
+})
